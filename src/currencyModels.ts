fd10e16cbe79b0e2984bea37c74d58589716dfd4
fesmofet <borees2@gmail.com>
2025-05-29 15:16:46 +0300

    1.0.3 version

--- src/currencyModels.ts
+++ src/currencyModels.ts
@@ -3,12 +3,12 @@ import CurrenciesRateSchema from "./currencies/CurrenciesRate/CurrenciesRateSche
 import CurrenciesStatisticSchema from "./currencies/CurrenciesStatistic/CurrenciesStatisticSchema";
 import HiveEngineRateSchema from "./currencies/HiveEngineRate/HiveEngineRateSchema";
 import ReservationCurrenciesSchema from "./currencies/ReservationCurrencies/ReservationCurrenciesSchema";
+import { ModelTuple } from "./types";
 
-
-const CurrenciesRateModel = [MODEL_NAME.CURRENCIES_RATE, CurrenciesRateSchema, COLLECTION_NAME.CURRENCIES_RATE];
-const CurrenciesStatisticModel = [MODEL_NAME.CURRENCIES_STATISTIC, CurrenciesStatisticSchema, COLLECTION_NAME.CURRENCIES_STATISTIC];
-const HiveEngineRateModel = [MODEL_NAME.HIVE_ENGINE_RATE, HiveEngineRateSchema, COLLECTION_NAME.HIVE_ENGINE_RATE];
-const ReservationCurrenciesModel = [MODEL_NAME.RESERVATION_CURRENCIES, ReservationCurrenciesSchema, COLLECTION_NAME.RESERVATION_CURRENCIES];
+const CurrenciesRateModel: ModelTuple = [MODEL_NAME.CURRENCIES_RATE, CurrenciesRateSchema, COLLECTION_NAME.CURRENCIES_RATE];
+const CurrenciesStatisticModel: ModelTuple = [MODEL_NAME.CURRENCIES_STATISTIC, CurrenciesStatisticSchema, COLLECTION_NAME.CURRENCIES_STATISTIC];
+const HiveEngineRateModel: ModelTuple = [MODEL_NAME.HIVE_ENGINE_RATE, HiveEngineRateSchema, COLLECTION_NAME.HIVE_ENGINE_RATE];
+const ReservationCurrenciesModel: ModelTuple = [MODEL_NAME.RESERVATION_CURRENCIES, ReservationCurrenciesSchema, COLLECTION_NAME.RESERVATION_CURRENCIES];
 
 const currencyModels = {
     CurrenciesRate: CurrenciesRateModel,
